e26ccc8ecbb06e274d5857181fa5602eaabdb6f2
henry hosken <henryhosken13@gmail.com>
2023-07-10 22:13:12 -0300

    .

---
+++ ControleDeContatos/Models/ContatoModels.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
