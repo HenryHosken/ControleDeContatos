07d3384eaeaea6bf8c5202f3a36a2300dd0dffd6
henry hosken <henryhosken13@gmail.com>
2023-08-02 22:14:50 -0300

    funcional

---
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeContatos.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Dgite o nome do usuário")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Dgite o login do usuário")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido!")]
+        public string Email { get; set; }
+        public PerfilEnum Perfil { get; set; }
+        [Required(ErrorMessage = "Dgite a senha do usuário")]
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
---
+++ ControleDeContatos/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,55 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public UsuarioRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public UsuarioModel ListarPorId(int id)
+        {
+            return _bancoContext.Usuarios.FirstOrDefault(x => x.Id == id);
+        }
+        public List<UsuarioModel> BuscarTodos()
+        {
+            return _bancoContext.Usuarios.ToList();
+        }
+        public UsuarioModel Adicionar(UsuarioModel usuario)
+        {
+            _bancoContext.Usuarios.Add(usuario);
+            _bancoContext.SaveChanges();
+            return usuario;
+        }
+
+        public UsuarioModel Atualizar(UsuarioModel usuario)
+        {
+            UsuarioModel usuarioDB = ListarPorId(usuario.Id);
+
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na atualização do contato!");
+
+            usuarioDB.Name = usuario.Name;
+            usuarioDB.Email = usuario.Email;
+            usuarioDB.Perfil = usuario.Perfil;
+            _bancoContext.Usuarios.Update(usuarioDB);
+            _bancoContext.SaveChanges();
+            
+            return usuarioDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            UsuarioModel usuarioDB = ListarPorId(id);
+            if (usuarioDB == null) throw new System.Exception("Houve um erro na deleção do contato!");
+            _bancoContext.Usuarios.Remove(usuarioDB);
+            _bancoContext.SaveChanges();
+            return true;
+        }
+    }
+}
