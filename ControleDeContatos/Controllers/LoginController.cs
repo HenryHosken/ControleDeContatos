7637abec4ad3fe6d3eefc49bf52f2cca54eb209d
henry hosken <henryhosken13@gmail.com>
2023-08-09 22:46:39 -0300

    Login com banco funcional

--- ControleDeContatos/Controllers/LoginController.cs
+++ ControleDeContatos/Controllers/LoginController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ControleDeContatos.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel) 
+        {
+            try
+            {
+                UsuarioModel usuario = _usuarioRepositorio.BuscarPorLogin(loginModel.Login);
+
+                if (ModelState.IsValid)
+                {
+                    if (usuario != null)
+                    {
+                        if (usuario.SenhaValida(loginModel.Login))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        TempData["MensagemErro"] = $"senha do usuario é inválida, tente novamente";
+
+                    }
+
+                    TempData["MensagemErro"] = $"Usuário ou senha inválido(s). Por favor, tente novamente";
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente, datalhe do erro {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
